ef254bfa43ca228b3267171d527395821604f5c7
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-20 10:38:27 +0500

    Created Sample DB and a PersonViewModel

---
+++ RTL Bootstrap Comprehensive Test/ViewModels/PersonVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTL_Bootstrap_Comprehensive_Test.ViewModels
+{
+    public class PersonVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="First Name is required.")]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public String EmailAddress { get; set; }
+    }
+}
