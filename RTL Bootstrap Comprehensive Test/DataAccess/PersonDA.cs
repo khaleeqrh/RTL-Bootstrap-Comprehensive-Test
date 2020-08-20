afb7599fc4ad409b595eaee8915217ff9bfaffc8
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-20 10:55:25 +0500

    Create PersonDA.cs

---
+++ RTL Bootstrap Comprehensive Test/DataAccess/PersonDA.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using RTL_Bootstrap_Comprehensive_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTL_Bootstrap_Comprehensive_Test.DataAccess
+{
+    public class PersonDA
+    {
+        public String getConnectionString(String name = "Default")
+        {
+            return "";
+        }
+
+        public List<PersonModel> GetPersons()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(getConnectionString()))                
+            {
+                return cnn.Query<PersonModel>("Select * From person").ToList();
+            }
+        }
+    }
+}
